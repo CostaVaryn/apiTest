51a3a6c9f9bcd03cfc8cd625d888aaf66f124f39
Costa Varyn <d3tug@yandex.ru>
2023-03-13 10:00:04 +0300

    refactor

--- src/test/java/tests/ReqresPojoTest.java
+++ src/test/java/tests/ReqresPojoTest.java
@@ -16,6 +16,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import static io.restassured.RestAssured.given;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
 public class ReqresPojoTest {
     private final static String URL = "https://reqres.in/";
@@ -26,15 +27,15 @@ public class ReqresPojoTest {
         List<UserData> users = given()
                 .when()
                 .get("api/users?page=2")
-                //.contentType(ContentType.JSON)
-                //.get(URL+"api/users?page=2")
+//                .contentType(ContentType.JSON)
+//                .get(URL + "api/users?page=2")
                 .then().log().all()
-                .extract().body().jsonPath().getList("data",UserData.class);
-        users.forEach(x-> Assert.assertTrue(x.getAvatar().contains(x.getId().toString())));
-        Assert.assertTrue(users.stream().allMatch(x->x.getEmail().endsWith("@reqres.in")));
+                .extract().body().jsonPath().getList("data", UserData.class);
+        users.forEach(x -> Assert.assertTrue(x.getAvatar().contains(x.getId().toString())));
+        Assert.assertTrue(users.stream().allMatch(x -> x.getEmail().endsWith("@reqres.in")));
         List<String> avatars = users.stream().map(UserData::getAvatar).collect(Collectors.toList());
-        List<String> ids = users.stream().map(x->x.getId().toString()).collect(Collectors.toList());
-        for (int i = 0; i<avatars.size(); i++){
+        List<String> ids = users.stream().map(x -> x.getId().toString()).collect(Collectors.toList());
+        for (int i = 0; i < avatars.size(); i++) {
             Assert.assertTrue(avatars.get(i).contains(ids.get(i)));
         }
     }
@@ -45,30 +46,32 @@ public class ReqresPojoTest {
         Specifications.installSpecification(Specifications.requestSpec(URL), Specifications.responseSpecOK200());
         Integer id = 4;
         String token = "QpwL5tke4Pnpja7X4";
-        Register user = new Register("eve.holt@reqres.in","pistol");
+        Register user = new Register("eve.holt@reqres.in", "pistol");
         SuccessReg successReg = given()
                 .body(user)
                 .when()
                 .post("api/register")
                 .then().log().all()
                 .extract().as(SuccessReg.class);
-        Assert.assertNotNull(successReg.getId());
-        Assert.assertNotNull(successReg.getToken());
-        Assert.assertEquals(id, successReg.getId());
-        Assert.assertEquals(token, successReg.getToken());
+        assertAll(
+                () -> Assert.assertNotNull(successReg.getId()),
+                () -> Assert.assertNotNull(successReg.getToken()),
+                () -> Assert.assertEquals(id, successReg.getId()),
+                () -> Assert.assertEquals(token, successReg.getToken())
+        );
     }
 
     @Test
     // тест неудачной регистрации
     public void unSuccessRegTest() {
         Specifications.installSpecification(Specifications.requestSpec(URL), Specifications.responseSpecError400());
-        Register user = new Register("sydney@fife","");
+        Register user = new Register("sydney@fife", "");
         UnSuccessReg unSuccessReg = given()
                 .body(user)
                 .post("api/register")
                 .then().log().all()
                 .extract().as(UnSuccessReg.class);
-        Assert.assertEquals("Missing password",unSuccessReg.getError());
+        Assert.assertEquals("Missing password", unSuccessReg.getError());
     }
 
     @Test
@@ -78,10 +81,10 @@ public class ReqresPojoTest {
                 .when()
                 .get("api/unknown")
                 .then().log().all()
-                .extract().body().jsonPath().getList("data",ColorsData.class);
+                .extract().body().jsonPath().getList("data", ColorsData.class);
         List<Integer> years = colors.stream().map(ColorsData::getYear).collect(Collectors.toList());
         List<Integer> sortedYears = years.stream().sorted().collect(Collectors.toList());
-        Assert.assertEquals(sortedYears,years);
+        Assert.assertEquals(sortedYears, years);
         System.out.println(years);
         System.out.println(sortedYears);
     }
@@ -106,9 +109,9 @@ public class ReqresPojoTest {
                 .then().log().all()
                 .extract().as(UserTimeResponse.class);
         String regex = "(.{6})$";
-        String currentTime = Clock.systemUTC().instant().toString().replaceAll(regex,"");
+        String currentTime = Clock.systemUTC().instant().toString().replaceAll(regex, "");
         System.out.println(currentTime);
-        Assert.assertEquals(currentTime, response.getUpdatedAt().replaceAll(regex,""));
-        System.out.println(response.getUpdatedAt().replaceAll(regex,""));
+        Assert.assertEquals(currentTime, response.getUpdatedAt().replaceAll(regex, ""));
+        System.out.println(response.getUpdatedAt().replaceAll(regex, ""));
     }
 }
