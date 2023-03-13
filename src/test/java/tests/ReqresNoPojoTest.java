26e2ca4e4753856ef72cdaf982fbefa2e0a68de9
Costa Varyn <d3tug@yandex.ru>
2023-03-13 09:59:45 +0300

    refactor and add test

--- src/test/java/tests/ReqresNoPojoTest.java
+++ src/test/java/tests/ReqresNoPojoTest.java
@@ -5,6 +5,7 @@ import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.jupiter.api.DisplayName;
 
 import java.util.HashMap;
 import java.util.List;
@@ -13,11 +14,16 @@ import java.util.Map;
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.notNullValue;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ReqresNoPojoTest {
     private final static String URL = "https://reqres.in/";
 
     @Test
+    @DisplayName("Check avatars")
     public void checkAvatarsNoPojoTest() {
         Specifications.installSpecification(Specifications.requestSpec(URL), Specifications.responseSpecOK200());
         Response response = given()
@@ -35,16 +41,47 @@ public class ReqresNoPojoTest {
         List<String> emails = jsonPath.get("data.email");
         List<Integer> ids = jsonPath.get("data.id");
         List<String> avatars = jsonPath.get("data.avatar");
-        for(int i = 0; i< avatars.size(); i++) {
-            Assert.assertTrue(avatars.get(i).contains(ids.get(i).toString()));
+        assertAll(
+                () -> assertNotNull(emails),
+                () -> assertNotNull(ids),
+                () -> assertNotNull(avatars),
+                () -> assertTrue(emails.stream().allMatch(x -> x.endsWith("@reqres.in")))
+        );
+        for (int i = 0; i < avatars.size(); i++) {
+            assertTrue(avatars.get(i).contains(ids.get(i).toString()));
         }
-
-        // проверка окончания почты на соответствующий домен
-        Assert.assertTrue(emails.stream().allMatch(x->x.endsWith("@reqres.in")));
     }
 
     @Test
-    public void successUserRegTestNoPojo() {
+    @DisplayName("Check single user")
+    public void checkSingleUserTest() {
+        Specifications.installSpecification(Specifications.requestSpec(URL), Specifications.responseSpecOK200());
+        Response response = given()
+                .when()
+                .get("api/users/2")
+                .then().log().all()
+                .body("data.id", notNullValue())
+                .body("data.email", notNullValue())
+                .body("data.first_name", notNullValue())
+                .body("data.last_name", notNullValue())
+                .body("data.avatar", notNullValue())
+                .extract().response();
+        JsonPath jsonPath = response.jsonPath();
+        int id = jsonPath.getInt("data.id");
+        String email = jsonPath.getString("data.email");
+        String firstName = jsonPath.getString("data.first_name");
+        String lastName = jsonPath.getString("data.last_name");
+        assertAll(
+                () -> assertEquals(2, id),
+                () -> assertEquals("janet.weaver@reqres.in", email),
+                () -> assertEquals("Janet", firstName),
+                () -> assertEquals("Weaver", lastName)
+        );
+    }
+
+    @Test
+    @DisplayName("Successful registration")
+    public void successfulUserRegTestNoPojo() {
         Specifications.installSpecification(Specifications.requestSpec(URL), Specifications.responseSpecOK200());
         Map<String, String> user = new HashMap<>();
         user.put("email", "eve.holt@reqres.in");
@@ -60,16 +97,18 @@ public class ReqresNoPojoTest {
         JsonPath jsonPath = response.jsonPath();
         int id = jsonPath.get("id");
         String token = jsonPath.get("token");
-        Assert.assertEquals(4, id);
-        Assert.assertEquals("QpwL5tke4Pnpja7X4", token);
+        assertAll(
+                () -> Assert.assertEquals(4, id),
+                () -> Assert.assertEquals("QpwL5tke4Pnpja7X4", token)
+        );
     }
 
     @Test
-    // Unsuccessful registration
-    public void unsuccessUserRegNoPojo() {
+    @DisplayName("Unsuccessful registration")
+    public void unsuccessfulUserRegNoPojo() {
         Specifications.installSpecification(Specifications.requestSpec(URL), Specifications.responseSpecError400());
         Map<String, String> user = new HashMap<>();
-        user.put("email","sydney@fife");
+        user.put("email", "sydney@fife");
         Response response = given()
                 .body(user)
                 .when()
