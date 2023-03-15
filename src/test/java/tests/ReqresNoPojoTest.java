438f5e4493c06e8cac42872691fff8d82ca71c27
Costa Varyn <d3tug@yandex.ru>
2023-03-15 15:16:10 +0300

    add tests

--- src/test/java/tests/ReqresNoPojoTest.java
+++ src/test/java/tests/ReqresNoPojoTest.java
@@ -1,16 +1,19 @@
 package tests;
 
+import api.reqres.info.PersonData;
+import api.reqres.info.ResourceData;
+import api.reqres.info.TestData;
 import api.spec.Specifications;
 import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Objects;
+import java.time.Clock;
+import java.util.*;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.equalTo;
@@ -21,18 +24,21 @@ import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ReqresNoPojoTest {
-    private final static String URL = "https://reqres.in/";
-    private final static String TOKEN = "QpwL5tke4Pnpja7X4";
+    private static final TestData testData = new TestData();
+    private final static String URL = testData.getUrl();
+    List<PersonData> usersList = new ArrayList<>(testData.getUsersList());
+    List<ResourceData> resList = new ArrayList<>(testData.getResList());
 
-    @Test
-    @DisplayName("Check avatars")
-    public void checkAvatarsNoPojoTest() {
+    @ParameterizedTest
+    @DisplayName("GET: Check list users")
+    @ValueSource(ints = {1, 2})
+    public void checkListUsersNoPojoTest(Integer page) {
         Specifications.installSpecification(Specifications.requestSpec(URL), Specifications.responseSpecOK200());
         Response response = given()
                 .when()
-                .get("api/users?page=2")
+                .get("api/users?page=" + page)
                 .then().log().all()
-                .body("page", equalTo(2))
+                .body("page", equalTo(page))
                 .body("data.id", notNullValue())
                 .body("data.email", notNullValue())
                 .body("data.first_name", notNullValue())
@@ -47,20 +53,21 @@ public class ReqresNoPojoTest {
                 () -> assertNotNull(emails),
                 () -> assertNotNull(ids),
                 () -> assertNotNull(avatars),
-                () -> assertTrue(emails.stream().allMatch(x -> x.endsWith("@reqres.in")))
+                () -> assertTrue(emails.stream().allMatch(x -> x.endsWith(testData.getDomainEmail())))
         );
         for (int i = 0; i < avatars.size(); i++) {
             assertTrue(avatars.get(i).contains(ids.get(i).toString()));
         }
     }
 
-    @Test
-    @DisplayName("Check single user")
-    public void checkSingleUserTest() {
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12})
+    @DisplayName("GET: Check single user")
+    public void checkSingleUserTest(Integer userId) {
         Specifications.installSpecification(Specifications.requestSpec(URL), Specifications.responseSpecOK200());
         Response response = given()
                 .when()
-                .get("api/users/2")
+                .get("api/users/" + userId)
                 .then().log().all()
                 .body("data.id", notNullValue())
                 .body("data.email", notNullValue())
@@ -74,15 +81,15 @@ public class ReqresNoPojoTest {
         String firstName = jsonPath.getString("data.first_name");
         String lastName = jsonPath.getString("data.last_name");
         assertAll(
-                () -> assertEquals(2, id),
-                () -> assertEquals("janet.weaver@reqres.in", email),
-                () -> assertEquals("Janet", firstName),
-                () -> assertEquals("Weaver", lastName)
+                () -> assertEquals(userId, id),
+                () -> assertEquals(usersList.get(userId - 1).getEmail(), email),
+                () -> assertEquals(usersList.get(userId - 1).getFirstName(), firstName),
+                () -> assertEquals(usersList.get(userId - 1).getLastName(), lastName)
         );
     }
 
     @Test
-    @DisplayName("Check single user not found")
+    @DisplayName("GET: Check single user not found")
     public void checkSingleUserNotFoundTest() {
         Specifications.installSpecification(Specifications.requestSpec(URL), Specifications.responseSpecUnique(404));
         Response response = given()
@@ -95,7 +102,7 @@ public class ReqresNoPojoTest {
     }
 
     @Test
-    @DisplayName("Check list resources")
+    @DisplayName("GET: Check list resources")
     public void checkListResourcesTest() {
         Specifications.installSpecification(Specifications.requestSpec(URL), Specifications.responseSpecOK200());
         Response response = given()
@@ -120,13 +127,14 @@ public class ReqresNoPojoTest {
         );
     }
 
-    @Test
-    @DisplayName("Check single resource")
-    public void checkSingleResourceTest() {
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12})
+    @DisplayName("GET: Check single resource")
+    public void checkSingleResourceTest(Integer resId) {
         Specifications.installSpecification(Specifications.requestSpec(URL), Specifications.responseSpecOK200());
         Response response = given()
                 .when()
-                .get("api/unknown/2")
+                .get("api/unknown/" + resId)
                 .then().log().all()
                 .body("data.id", notNullValue())
                 .body("data.name", notNullValue())
@@ -139,14 +147,14 @@ public class ReqresNoPojoTest {
         String name = jsonPath.getString("data.name");
         int year = jsonPath.getInt("data.year");
         assertAll(
-                () -> assertEquals(2, id),
-                () -> assertEquals("fuchsia rose", name),
-                () -> assertEquals(2001, year)
+                () -> assertEquals(resId, id),
+                () -> assertEquals(resList.get(resId - 1).getResName(), name),
+                () -> assertEquals(resList.get(resId - 1).getResYear(), year)
         );
     }
 
     @Test
-    @DisplayName("Check single resource not found")
+    @DisplayName("GET: Check single resource not found")
     public void checkSingleResourceNotFoundTest() {
         Specifications.installSpecification(Specifications.requestSpec(URL), Specifications.responseSpecUnique(404));
         Response response = given()
@@ -159,35 +167,101 @@ public class ReqresNoPojoTest {
     }
 
     @Test
-    @DisplayName("Successful registration")
-    public void successfulUserRegTestNoPojo() {
+    @DisplayName("GET: Delayed response")
+    public void checkDelayedResponseTest() {
+        Specifications.installSpecification(Specifications.requestSpec(URL), Specifications.responseSpecOK200());
+        Response response = given()
+                .when()
+                .get("/api/users?delay=3")
+                .then()
+                .log().all()
+                .body("page", equalTo(1))
+                .body("data.id", notNullValue())
+                .body("data.email", notNullValue())
+                .body("data.first_name", notNullValue())
+                .body("data.last_name", notNullValue())
+                .body("data.avatar", notNullValue())
+                .statusCode(200)
+                .extract().response();
+        JsonPath jsonPath = response.jsonPath();
+        List<String> emails = jsonPath.get("data.email");
+        List<Integer> ids = jsonPath.get("data.id");
+        List<String> avatars = jsonPath.get("data.avatar");
+        assertAll(
+                () -> assertEquals(200, response.getStatusCode()),
+                () -> assertNotNull(emails),
+                () -> assertNotNull(ids),
+                () -> assertNotNull(avatars),
+                () -> assertTrue(emails.stream().allMatch(x -> x.endsWith(testData.getDomainEmail())))
+        );
+    }
+
+    @Test
+    @DisplayName("POST: Successful registration")
+    public void successfulUserRegNoPojoTest() {
         Specifications.installSpecification(Specifications.requestSpec(URL), Specifications.responseSpecOK200());
         Map<String, String> user = new HashMap<>();
-        user.put("email", "eve.holt@reqres.in");
-        user.put("password", "pistol");
+        user.put("email", testData.getValidEmail());
+        user.put("password", testData.getRegPass());
         Response response = given()
                 .body(user)
                 .when()
                 .post("api/register")
                 .then().log().all()
                 .body("id", equalTo(4))
-                .body("token", equalTo(TOKEN))
+                .body("token", equalTo(testData.getToken()))
                 .extract().response();
         JsonPath jsonPath = response.jsonPath();
         int id = jsonPath.get("id");
         String token = jsonPath.get("token");
         assertAll(
                 () -> Assert.assertEquals(4, id),
-                () -> Assert.assertEquals(TOKEN, token)
+                () -> Assert.assertEquals(testData.getToken(), token)
         );
     }
 
     @Test
-    @DisplayName("Unsuccessful registration")
-    public void unsuccessfulUserRegNoPojo() {
+    @DisplayName("POST: Successful authorization")
+    public void successfulUserAuthNoPojoTest() {
+        Specifications.installSpecification(Specifications.requestSpec(URL), Specifications.responseSpecOK200());
+        Map<String, String> user = new HashMap<>();
+        user.put("email", testData.getValidEmail());
+        user.put("password", testData.getLogPass());
+        Response response = given()
+                .body(user)
+                .when()
+                .post("api/login")
+                .then().log().all()
+                .body("token", equalTo(testData.getToken()))
+                .extract().response();
+        JsonPath jsonPath = response.jsonPath();
+        String token = jsonPath.get("token");
+        assertEquals(testData.getToken(), token);
+    }
+
+    @Test
+    @DisplayName("POST: Unsuccessful authorization")
+    public void unsuccessfulUserAuthNoPojoTest() {
         Specifications.installSpecification(Specifications.requestSpec(URL), Specifications.responseSpecError400());
         Map<String, String> user = new HashMap<>();
-        user.put("email", "sydney@fife");
+        user.put("email", testData.getInvalidLogEmail());
+        Response response = given()
+                .body(user)
+                .when()
+                .post("api/login")
+                .then().log().all()
+                .extract().response();
+        JsonPath jsonPath = response.jsonPath();
+        String error = jsonPath.get("error");
+        Assert.assertEquals(testData.getErrorMessage(), error);
+    }
+
+    @Test
+    @DisplayName("POST: Unsuccessful registration")
+    public void unsuccessfulUserRegNoPojoTest() {
+        Specifications.installSpecification(Specifications.requestSpec(URL), Specifications.responseSpecError400());
+        Map<String, String> user = new HashMap<>();
+        user.put("email", testData.getInvalidRegEmail());
         Response response = given()
                 .body(user)
                 .when()
@@ -196,6 +270,109 @@ public class ReqresNoPojoTest {
                 .extract().response();
         JsonPath jsonPath = response.jsonPath();
         String error = jsonPath.get("error");
-        Assert.assertEquals("Missing password", error);
+        Assert.assertEquals(testData.getErrorMessage(), error);
+    }
+
+    @Test
+    @DisplayName("POST: Create resource")
+    public void checkCreateResourceNoPojoTest() {
+        Specifications.installSpecification(Specifications.requestSpec(URL), Specifications.responseSpecUnique(201));
+        Map<String, String> jsonBody = new HashMap<>();
+        jsonBody.put("name", testData.getNameTest());
+        jsonBody.put("job", testData.getJobTest());
+        Response response = given()
+                .body(jsonBody)
+                .when()
+                .post("api/users")
+                .then().log().all()
+                .body("name", equalTo(testData.getNameTest()))
+                .body("job", equalTo(testData.getJobTest()))
+                .body("id", notNullValue())
+                .body("createdAt", notNullValue())
+                .extract().response();
+        JsonPath jsonPath = response.jsonPath();
+        String name = jsonPath.getString("name");
+        String job = jsonPath.getString("job");
+        int id = Integer.parseInt(jsonPath.getString("id"));
+        String regex = "(.{6})$";
+        String createdAt = jsonPath.getString("createdAt").replaceAll(regex, "");
+        String currentTime = Clock.systemUTC().instant().toString().replaceAll(regex, "");
+        assertAll(
+                () -> assertEquals(testData.getNameTest(), name),
+                () -> assertEquals(testData.getJobTest(), job),
+                () -> assertTrue(id > 0),
+                () -> assertEquals(currentTime, createdAt)
+        );
+    }
+
+    @Test
+    @DisplayName("PUT: Update resource")
+    public void checkPutUpdateResourceNoPojoTest() {
+        Specifications.installSpecification(Specifications.requestSpec(URL), Specifications.responseSpecOK200());
+        Map<String, String> jsonBody = new HashMap<>();
+        jsonBody.put("name", testData.getNameTest());
+        jsonBody.put("job", testData.getJobUpdateTest());
+        Response response = given()
+                .body(jsonBody)
+                .when()
+                .put("api/users/1")
+                .then().log().all()
+                .body("name", equalTo(testData.getNameTest()))
+                .body("job", equalTo(testData.getJobUpdateTest()))
+                .body("updatedAt", notNullValue())
+                .extract().response();
+        JsonPath jsonPath = response.jsonPath();
+        String name = jsonPath.getString("name");
+        String job = jsonPath.getString("job");
+        String regex = "(.{6})$";
+        String updatedAt = jsonPath.getString("updatedAt").replaceAll(regex, "");
+        String currentTime = Clock.systemUTC().instant().toString().replaceAll(regex, "");
+        assertAll(
+                () -> assertEquals(testData.getNameTest(), name),
+                () -> assertEquals(testData.getJobUpdateTest(), job),
+                () -> assertEquals(currentTime, updatedAt)
+        );
+    }
+
+    @Test
+    @DisplayName("PATCH: Update resource")
+    public void checkPatchUpdateResourceNoPojoTest() {
+        Specifications.installSpecification(Specifications.requestSpec(URL), Specifications.responseSpecOK200());
+        Map<String, String> jsonBody = new HashMap<>();
+        jsonBody.put("name", testData.getNameTest());
+        jsonBody.put("job", testData.getJobUpdateTest());
+        Response response = given()
+                .body(jsonBody)
+                .when()
+                .patch("api/users/2")
+                .then().log().all()
+                .body("name", equalTo(testData.getNameTest()))
+                .body("job", equalTo(testData.getJobUpdateTest()))
+                .body("updatedAt", notNullValue())
+                .extract().response();
+        JsonPath jsonPath = response.jsonPath();
+        String name = jsonPath.getString("name");
+        String job = jsonPath.getString("job");
+        String regex = "(.{6})$";
+        String updatedAt = jsonPath.getString("updatedAt").replaceAll(regex, "");
+        String currentTime = Clock.systemUTC().instant().toString().replaceAll(regex, "");
+        assertAll(
+                () -> assertEquals(testData.getNameTest(), name),
+                () -> assertEquals(testData.getJobUpdateTest(), job),
+                () -> assertEquals(currentTime, updatedAt)
+        );
+    }
+
+    @Test
+    @DisplayName("DELETE: Delete user")
+    public void deleteUserNoPojoTest() {
+        Specifications.installSpecification(Specifications.requestSpec(URL), Specifications.responseSpecUnique(204));
+        Response response = given()
+                .when()
+                .delete("api/users/2")
+                .then().log().all()
+                .statusCode(204)
+                .extract().response();
+        assertEquals(204, response.getStatusCode());
     }
 }
