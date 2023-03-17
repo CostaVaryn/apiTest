30e3ae8d7496a4fd86477588b2ed425d680fdaf9
Costa Varyn <d3tug@yandex.ru>
2023-03-17 10:59:49 +0300

    refactor

--- src/test/java/tests/ReqresPojoTest.java
+++ src/test/java/tests/ReqresPojoTest.java
@@ -8,6 +8,7 @@ import api.reqres.users.UserData;
 import api.reqres.users.UserTime;
 import api.reqres.users.UserTimeResponse;
 import api.spec.Specifications;
+import io.restassured.response.Response;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -16,6 +17,8 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import static io.restassured.RestAssured.given;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertAll;
 
 public class ReqresPojoTest {
@@ -41,37 +44,35 @@ public class ReqresPojoTest {
     }
 
     @Test
-    // тест успешной регистрации
     public void successRegTest() {
         Specifications.installSpecification(Specifications.requestSpec(URL), Specifications.responseSpecOK200());
         Integer id = 4;
         String token = "QpwL5tke4Pnpja7X4";
         Register user = new Register("eve.holt@reqres.in", "pistol");
         SuccessReg successReg = given()
-                .body(user)
+                .body(user.toString())
                 .when()
                 .post("api/register")
                 .then().log().all()
                 .extract().as(SuccessReg.class);
         assertAll(
-                () -> Assert.assertNotNull(successReg.getId()),
-                () -> Assert.assertNotNull(successReg.getToken()),
-                () -> Assert.assertEquals(id, successReg.getId()),
-                () -> Assert.assertEquals(token, successReg.getToken())
+                () -> assertNotNull(successReg.getId()),
+                () -> assertNotNull(successReg.getToken()),
+                () -> assertEquals(id, successReg.getId()),
+                () -> assertEquals(token, successReg.getToken())
         );
     }
 
     @Test
-    // тест неудачной регистрации
     public void unSuccessRegTest() {
         Specifications.installSpecification(Specifications.requestSpec(URL), Specifications.responseSpecError400());
         Register user = new Register("sydney@fife", "");
         UnSuccessReg unSuccessReg = given()
-                .body(user)
+                .body(user.toString())
                 .post("api/register")
                 .then().log().all()
                 .extract().as(UnSuccessReg.class);
-        Assert.assertEquals("Missing password", unSuccessReg.getError());
+        assertEquals("Missing password", unSuccessReg.getError());
     }
 
     @Test
@@ -84,22 +85,21 @@ public class ReqresPojoTest {
                 .extract().body().jsonPath().getList("data", ColorsData.class);
         List<Integer> years = colors.stream().map(ColorsData::getYear).collect(Collectors.toList());
         List<Integer> sortedYears = years.stream().sorted().collect(Collectors.toList());
-        Assert.assertEquals(sortedYears, years);
-        System.out.println(years);
-        System.out.println(sortedYears);
+        assertEquals(sortedYears, years);
     }
 
     @Test
-    public void deleteUserTest() {
+    public void checkDeleteUserTest() {
         Specifications.installSpecification(Specifications.requestSpec(URL), Specifications.responseSpecUnique(204));
-        given()
+        Response response = given()
                 .when()
                 .delete("api/users/2")
-                .then().log().all();
+                .then().log().all().extract().response();
+        assertEquals(204, response.getStatusCode());
     }
 
     @Test
-    public void timeTest() {
+    public void checkTimeUpdatedTest() {
         Specifications.installSpecification(Specifications.requestSpec(URL), Specifications.responseSpecOK200());
         UserTime user = new UserTime("morpheus", "zion resident");
         UserTimeResponse response = given()
@@ -110,8 +110,6 @@ public class ReqresPojoTest {
                 .extract().as(UserTimeResponse.class);
         String regex = "(.{6})$";
         String currentTime = Clock.systemUTC().instant().toString().replaceAll(regex, "");
-        System.out.println(currentTime);
-        Assert.assertEquals(currentTime, response.getUpdatedAt().replaceAll(regex, ""));
-        System.out.println(response.getUpdatedAt().replaceAll(regex, ""));
+        assertEquals(currentTime, response.getUpdatedAt().replaceAll(regex, ""));
     }
 }
