7465cf782375a0718581f4adeade2f2b2bffcae4
Costa Varyn <d3tug@yandex.ru>
2023-03-24 13:52:08 +0300

    add login class

---
+++ src/main/java/api/reqres/auth/Login.java
@@ -0,0 +1,17 @@
+package api.reqres.auth;
+
+public class Login {
+    private String email;
+    private String password;
+
+    public Login(String email, String password) {
+        this.email = email;
+        this.password = password;
+    }
+
+    public String toString() {
+        return password.equals("") ? "{\n    " + "\"email\": " + "\"" + email +"\"" + "\n" + "}" :
+                "{\n    " + "\"email\": " + "\"" + email +"\"," + "\n" +
+                        "    " + "\"password\": " + "\"" + password + "\"\n" + "}";
+    }
+}
