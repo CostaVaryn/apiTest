fb97ea707e8473edff557acc51b6854ba683f589
Costa Varyn <d3tug@yandex.ru>
2023-03-25 18:06:45 +0300

    add fields, methods and constructor

--- src/main/java/api/reqres/users/UserTimeResponse.java
+++ src/main/java/api/reqres/users/UserTimeResponse.java
@@ -1,16 +1,31 @@
 package api.reqres.users;
 
 public class UserTimeResponse extends UserTime {
+    private String id;
     private String updatedAt;
+    private String createdAt;
 
     public UserTimeResponse(String name, String job, String updatedAt) {
         super(name, job);
         this.updatedAt = updatedAt;
     }
+
+    public UserTimeResponse(String name, String job, String id, String createdAt) {
+        super(name, job);
+        this.id = id;
+        this.createdAt = createdAt;
+    }
     public UserTimeResponse() {
     }
-    
+
+    public String getId() {
+        return id;
+    }
+
     public String getUpdatedAt() {
         return updatedAt;
+    }
+    public String getCreatedAt() {
+        return createdAt;
     }
 }
