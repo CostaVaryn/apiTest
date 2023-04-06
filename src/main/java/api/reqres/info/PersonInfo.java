4ad64283eb11987ce260f51c009ea5111a2d6111
Constantin Varyn <d3tug@yandex.ru>
2023-04-06 11:47:09 +0300

    refactor: rename

---
+++ src/main/java/api/reqres/info/PersonInfo.java
@@ -0,0 +1,25 @@
+package api.reqres.info;
+
+public class PersonInfo {
+    String email;
+    String firstName;
+    String lastName;
+
+    public PersonInfo(String email, String firstName, String lastName) {
+        this.email = email;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+}
